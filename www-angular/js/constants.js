66adf1e13b1e73451d56cc1b792031d83774eac9
Stanek Keith - kstane <kstane@kstane0415mac.corp.acxiom.net>
2015-11-19 16:19:51 -0600

    cleanup of some unnecessary directories

--- www-angular/js/constants.js
+++
@@ -1,9 +0,0 @@
-// global variables
-var condimentDictionary = [];
-
-var categoryPanelBgColor = "fcf8e3";
-var categoryHeaderBgColor = "337ab7";
-var categoryHeaderFontColor = "ffffff";
-var itemPanelBgColor = "fff";
-var itemHeaderBgColor = "79A7CD"; //"00aeef";
-var itemHeaderFontColor = "fff";
