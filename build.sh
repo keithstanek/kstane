902d060db091bd824d74e05e0d5a1a020ab45a11
Stanek Keith - kstane <kstane@kstane0415mac.corp.acxiom.net>
2015-12-30 12:19:53 -0600

    removing script files

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-directory=$1
-buildNo=$2
-
-echo "directory [$directory] buildNo [$buildNo]"
-echo ""
-
-zip -r "$directory-$buildNo.zip" $directory/ -x "$directory/platforms/*" "$directory/.git/*"
